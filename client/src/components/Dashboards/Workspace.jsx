62bb914dc42bbad3fbddff3f2a23dc71fbe10060
Can Aktaran <canaktaran@Cans-MacBook-Pro.local>
2023-06-15 13:51:58 +0200

    added getDashboards Function to Workspace

--- client/src/components/Dashboards/Workspace.jsx
+++ client/src/components/Dashboards/Workspace.jsx
@@ -3,12 +3,31 @@ import { useHistory } from "react-router-dom";
 
 const Workspace = () => {
 
-    return (
-        <div>
-            <h1>Workspace</h1>
-            <button>New Dashboard</button>
-        </div>
-    )
+    const [dashboards, setDashboards] = useState([]);
+
+    const getDashboards = async () => {
+        const response = await fetch(
+            "http://localhost:5555/dashboards/",
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + window.localStorage.getItem("token"),
+                },
+            }
+        )
+
+        const jsonResponse = await response.json();
+        setDashboards(jsonResponse);
+        }
+}
+
+return (
+    <div>
+        <h1>Workspace</h1>
+        <button>New Dashboard</button>
+    </div>
+)
 
 }
 
