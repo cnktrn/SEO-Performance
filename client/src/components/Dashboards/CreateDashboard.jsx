319154ff7c53b3064ad939988bcadd4c5cec2bfc
Can Aktaran <canaktaran@Cans-MacBook-Pro.local>
2023-06-15 16:51:38 +0200

    added Dropdown Menu to select data source

--- client/src/components/Dashboards/CreateDashboard.jsx
+++ client/src/components/Dashboards/CreateDashboard.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
+
 const CreateDashboard = () => {
 
     const [kpiList, setKpiList] = useState();
     const [dashboardName, setDashboardName] = useState("");
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
 
     const getKpiList = async () => {
         const response = await fetch(
@@ -22,8 +28,20 @@ const CreateDashboard = () => {
                     type={"text"}
                     onChange={e => setDashboardName(e.target.value)}
                 />
+                <h3>Available SEO Metrics</h3>
             </div>
-            <div className="right-column"></div>
+            <div className="right-column">
+                <h3>Data Source</h3>
+                <div className="dropdown-menu">
+                    <select value={selectedOption} onChange={handleOptionChange}>
+                        <option value="">Select an option</option>
+                        <option value="option1">Option 1</option>
+                        <option value="option2">Option 2</option>
+                        <option value="option3">Option 3</option>
+                    </select>
+                </div>
+                <h3>Your Dashboard</h3>
+            </div>
         </div>
     )
 }
