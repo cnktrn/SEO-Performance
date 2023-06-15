636b47bbf793e34c4238b538d0ab7a223e593414
Can Aktaran <canaktaran@Cans-MacBook-Pro.local>
2023-06-15 16:42:41 +0200

    added Name Input Field to CreateDashboard Page

--- client/src/components/Dashboards/CreateDashboard.jsx
+++ client/src/components/Dashboards/CreateDashboard.jsx
@@ -3,17 +3,27 @@ import { useState } from "react";
 const CreateDashboard = () => {
 
     const [kpiList, setKpiList] = useState();
+    const [dashboardName, setDashboardName] = useState("");
 
     const getKpiList = async () => {
-        const response = await fetch (
+        const response = await fetch(
             ""
         )
     }
-    
+
 
     return (
         <div>
             <h1>Create Dashboard</h1>
+            <div className="left-column">
+                <h3>Name</h3>
+                <input
+                    value={dashboardName}
+                    type={"text"}
+                    onChange={e => setDashboardName(e.target.value)}
+                />
+            </div>
+            <div className="right-column"></div>
         </div>
     )
 }
